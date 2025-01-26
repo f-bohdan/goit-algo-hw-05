a6eaf59554a160fd202104866d08ef51fa5980be
f-bohdan <fliakbohdan@gmail.com>
2025-01-26 13:48:28 +0000

    commit

--- taks1.py
+++ taks1.py
@@ -1,1 +1,23 @@
-# hello its 5 module
+def caching_fibonacci():
+    # Створення порожнього словника cache
+    cache = dict()
+    # Cтворення внустрішньої функції fibonacci
+    def fibonacci(n):
+        if n <= 0:
+            return 0
+        elif n == 1:
+            return 1
+        elif n in cache:
+            return cache[n]
+        # якщо запис не існує в кеші, то створюємо його
+        cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
+        return cache[n]
+
+    return fibonacci
+
+# Отримуємо функцію fibonacci
+fib = caching_fibonacci()
+
+# Використовуємо функцію fibonacci для обчислення чисел Фібоначчі
+print(fib(10))  # Виведе 55
+print(fib(15))  # Виведе 610
